0631363dd7bc1cb7d7f2117a1d52b74044445797
Farook <farook.ai.prompt.engineer@gmail.com>
2026-06-27 04:45:26 -0700

    lesson 6 logical operators

---
+++ logical.py
@@ -0,0 +1,16 @@
+age = 25
+salary = 2500000
+
+if age >= 18 and salary >= 2000000:
+    print("AI Engineer")
+
+cpu = 95
+memory = 60
+
+if cpu > 90 or memory > 90:
+    print("Server Alert")
+
+is_student = False
+
+if not is_student:
+    print("Employee")
